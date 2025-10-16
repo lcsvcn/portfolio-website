e9fb53eb7de765234421892ba7c3c77b143cf030
lcsvcn <6011385+lcsvcn@users.noreply.github.com>
2025-10-16 18:26:45 -0300

    added posthog

--- src/index.js
+++ src/index.js
@@ -3,10 +3,25 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { createRoot } from 'react-dom/client';
+import { PostHogProvider } from 'posthog-js/react';
 
 const container = document.getElementById("root");
-const root = createRoot(container); 
-root.render(<App />);
+const root = createRoot(container);
+root.render(
+  <React.StrictMode>
+    <PostHogProvider
+      apiKey={process.env.REACT_APP_POSTHOG_KEY}
+      options={{
+        api_host: process.env.REACT_APP_POSTHOG_HOST,
+        defaults: '2025-05-24',
+        capture_exceptions: true, // This enables capturing exceptions using Error Tracking
+        debug: process.env.NODE_ENV === "development",
+      }}
+    >
+      <App />
+    </PostHogProvider>
+  </React.StrictMode>
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
