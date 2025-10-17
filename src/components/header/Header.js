53685f3d862d1253451eaa0a43f6cce454eaac15
lcsvcn <6011385+lcsvcn@users.noreply.github.com>
2025-10-17 11:05:14 -0300

    added center icon and sticky header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,48 +1,43 @@
 import "./Header.css";
-import { Fade } from "react-awesome-reveal";
 import { greeting, workExperiences } from "../../portfolio";
 import DarkModeToggle from "../darkModeToggle/DarkModeToggle";
 
 function Header() {
   const exp = workExperiences.viewExperiences;
   return (
-    <Fade top duration={1000} distance="20px">
-      <div>
-        <header className="header">
-          <a href="/" className="logo">
-            <span className="grey-color"> &lt;</span>
-            <span className="logo-name">{greeting.username}</span>
-            <span className="grey-color">/&gt;</span>
-          </a>
-          <input className="menu-btn" type="checkbox" id="menu-btn" />
-          <label className="menu-icon" htmlFor="menu-btn">
-            <span className="navicon"></span>
-          </label>
-          <ul className="menu">
-            <li>
-              <a href="#skills">Skills</a>
-            </li>
-            {exp === true && (
-              <li>
-                <a href="#experience">Work Experiences</a>
-              </li>
-            )}
-            <li>
-              <a href="#opensource">Open Source</a>
-            </li>
-            <li>
-              <a href="#projects">Projects</a>
-            </li>
-            <li>
-              <a href="#contact">Contact Me</a>
-            </li>
-            <li className="theme-toggle-item">
-              <DarkModeToggle />
-            </li>
-          </ul>
-        </header>
-      </div>
-    </Fade>
+    <header className="header">
+      <a href="/" className="logo">
+        <span className="grey-color"> &lt;</span>
+        <span className="logo-name">{greeting.username}</span>
+        <span className="grey-color">/&gt;</span>
+      </a>
+      <input className="menu-btn" type="checkbox" id="menu-btn" />
+      <label className="menu-icon" htmlFor="menu-btn">
+        <span className="navicon"></span>
+      </label>
+      <ul className="menu">
+        <li>
+          <a href="#skills">Skills</a>
+        </li>
+        {exp === true && (
+          <li>
+            <a href="#experience">Work Experiences</a>
+          </li>
+        )}
+        <li>
+          <a href="#opensource">Open Source</a>
+        </li>
+        <li>
+          <a href="#projects">Projects</a>
+        </li>
+        <li>
+          <a href="#contact">Contact Me</a>
+        </li>
+        <li className="theme-toggle-item">
+          <DarkModeToggle />
+        </li>
+      </ul>
+    </header>
   );
 }
 export default Header;
